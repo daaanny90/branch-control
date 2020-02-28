89da0988e0ff2a29b2b515e785a828113c0cf33e
3m5.Danny Spina <Danny.Spina@3m5.de>
2020-02-28 16:56:40 +0100

    First Commit

---
+++ spec/branch-control-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BranchControlView from '../lib/branch-control-view';
+
+describe('BranchControlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
